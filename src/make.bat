cc3ea7c81ce306062610e5e26c41fd4a11b50b4c
shaw02 <sha_w@nifty.com>
2008-11-13 18:29:48 +0900

    Version 1.04

---
+++ src/make.bat
@@ -0,0 +1,7 @@
+rem path c:\Program files\Microsoft Visual Studio 8\VC\bin\;C:\Program Files\Microsoft Visual Studio 8\Common7\IDE
+
+path c:\masm611\bin;c:\masm611\binr
+
+masm /Ml /Mx ff8mml.asm; >merr.txt
+link /tiny ff8mml.obj; >lerr.txt
+
