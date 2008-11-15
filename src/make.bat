0d5a4c2bdbfb8d61201a4e38ffa75e766ba0922a
shaw02 <sha_w@nifty.com>
2008-11-15 18:41:55 +0900

    Version 1.06

--- src/make.bat
+++ src/make.bat
@@ -2,6 +2,10 @@ rem path c:\Program files\Microsoft Visual Studio 8\VC\bin\;C:\Program Files\Mic
 
 path c:\masm611\bin;c:\masm611\binr
 
-masm /Ml /Mx ff8mml.asm; >merr.txt
-link /tiny ff8mml.obj; >lerr.txt
+masm /Ml /Mx /DFF7=1 akao2mml.asm,ff7mml.obj; >merrff7.txt
+masm /Ml /Mx /DFF8=1 akao2mml.asm,ff8mml.obj; >merrff8.txt
+
+link /tiny ff7mml.obj; >lerrf77.txt
+link /tiny ff8mml.obj; >lerrff8.txt
+
 
